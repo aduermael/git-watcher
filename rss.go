b2dfac9faa0cdca643d7626531ed8b7b620c2c7a
Adrien Duermael <adrien@duermael.com>
2017-05-12 15:57:37 -0700

    fix rss
    Signed-off-by: Adrien Duermael <adrien@duermael.com>

--- rss.go
+++ rss.go
@@ -12,7 +12,9 @@ func diffToRSSFeed(title, desc string, diffs []*Diff) {
 
 	feed := &feeds.Feed{
 		Title:       "Git repo watcher",
+		Link:        &feeds.Link{Href: ""},
 		Description: "Changes from watched Git repositories.",
+		Author:      &feeds.Author{Name: "Jason Moiron", Email: "jmoiron@jmoiron.net"},
 		Created:     now,
 		Items:       make([]*feeds.Item, 0),
 	}
@@ -20,12 +22,13 @@ func diffToRSSFeed(title, desc string, diffs []*Diff) {
 	item := &feeds.Item{
 		Title:       "changes",
 		Link:        &feeds.Link{Href: "https://github.com"},
-		Description: "Changes:\n",
+		Description: "Changes:<br />",
+		Author:      &feeds.Author{Name: "Jason Moiron", Email: "jmoiron@jmoiron.net"},
 		Created:     now,
 	}
 
 	for _, diff := range diffs {
-		item.Description += string(diff.Type) + " - " + diff.File + "\n"
+		item.Description += string(diff.Type) + " - " + diff.File + "<br />"
 	}
 
 	feed.Items = append(feed.Items, item)
